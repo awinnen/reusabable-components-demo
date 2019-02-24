57f11643a3f793546c9ae762a2c96c908b4e2d5f
alexander.winnen@lise.de <alexander.winnen@lise.de>
2019-02-24 17:00:43 +0100

    relative paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgContentComponent } from './examples/ng-content/ng-content.component';
-import { NgContentModule } from 'src/app/examples/ng-content/ng-content.module';
-import { NgTemplateModule } from 'src/app/examples/ng-template/ng-template.module';
+import { NgContentModule } from './examples/ng-content/ng-content.module';
+import { NgTemplateModule } from './examples/ng-template/ng-template.module';
 
 @NgModule({
   declarations: [
