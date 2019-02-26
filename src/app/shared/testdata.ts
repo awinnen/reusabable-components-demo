193788a84c0bcc92e7dce756a8e2e59d43f34415
alexander.winnen@lise.de <alexander.winnen@lise.de>
2019-02-26 13:39:16 +0100

    typo

--- src/app/shared/testdata.ts
+++ src/app/shared/testdata.ts
@@ -1,10 +1,10 @@
 export const beverages: Beverage[] = [
-    { name: "Cola", ingredients: ["sugar", "coffeine", "water"], fillingQuantity: "1ltr", price: 1, description: "a nice brown drink", color: "brown" },
+    { name: "Cola", ingredients: ["sugar", "caffeine", "water"], fillingQuantity: "1ltr", price: 1, description: "a nice brown drink", color: "brown" },
     { name: "Orange Juice", ingredients: ["Orange Juice", "sugar"], fillingQuantity: "500ml", price: 1.25, description: "made from spanish oranges", color: "orange" },
     { name: "Beer", ingredients: ["water", "hop"], fillingQuantity: "33cl", price: 1.7, description: "Great beer imported from Germany", color: "orange" }, 
     { name: "Milk", ingredients: ["water"], fillingQuantity: "1ltr", price: 0.58, description: "muuuuh", color: "white" },
     { name: "Soda", ingredients: ["water", "sugar", "lemonjuice"], fillingQuantity: "0.5 ltr", price: 0.99, description: "a fresh drink which fits any time", color: "yellow" },
-    { name: "MilkCoffee", ingredients: ["milk", "coffee", "sugar", "coffeine"], fillingQuantity: "0.2 ltr", price: 1.99, description: "a nice brown drink for breakfast", color: "brown" },
+    { name: "MilkCoffee", ingredients: ["milk", "coffee", "sugar", "caffeine"], fillingQuantity: "0.2 ltr", price: 1.99, description: "a nice brown drink for breakfast", color: "brown" },
     { name: "tee", ingredients: ["water", "tea"], fillingQuantity: "1 cup", price: 0.7, description: "a nice drink for all who dont want to sleep!", color: "green" }
 ];
 
